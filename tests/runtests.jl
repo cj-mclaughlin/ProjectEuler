f2699eab37635872c29a55764208d52dfe53c9f0
Connor McLaughlin <46900583+cj-mclaughlin@users.noreply.github.com>
2019-05-23 18:29:28 -0400

    trying out travis

--- tests/runtests.jl
+++ tests/runtests.jl
@@ -8,12 +8,10 @@ include("C:/Users/Connor/github/ProjectEuler/src/p05.jl")
 # sanity check
 @test 1 == 1
 
-@test p1_fast() == 233168
-
-@test p2() == 4613732
-
-@test p3(600851475143) == 6857
-
-@test p4() == 906609
-
-@test p5(1, 20) == 232792560
+@testset "first five problems" begin
+    @test p1_fast() == 233168
+    @test p2() == 4613732
+    @test p3(600851475143) == 6857
+    @test p4() == 906609
+    @test p5(1, 20) == 232792560
+end
