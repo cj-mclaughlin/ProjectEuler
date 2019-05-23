7aef9b8eaa89c27a4a20bd986f306a05257e614a
Connor McLaughlin <46900583+cj-mclaughlin@users.noreply.github.com>
2019-05-22 22:41:09 -0400

    problem 1

---
+++ p1.jl
@@ -0,0 +1,24 @@
+# Project Euler Problem 1 - Connor Mclaughlin
+# naive solution
+function p1()
+    solution = 0
+    for i = 1:999
+        if i % 3 == 0 || i % 5 == 0
+            solution += i
+        end
+    end
+    return solution
+end
+
+println(p1())
+
+# a better approach
+function sum_multiples_below(multiple, upper_bound)
+    solution = 0
+    for i = 0:multiple:upper_bound
+        solution += i
+    end
+    return solution
+end
+
+println(sum_multiples_below(3, 999) + sum_multiples_below(5, 999) - sum_multiples_below(15, 999))
